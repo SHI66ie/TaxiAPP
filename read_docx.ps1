8b0c30f96ef497efabb0960ced4244603f926a64
SHI66ie <ybarkido@gmail.com>
2026-08-07 11:25:04 +0100

    Create read_docx.ps1

---
+++ read_docx.ps1
@@ -0,0 +1,9 @@
+$zip = [System.IO.Compression.ZipFile]::OpenRead((Resolve-Path 'taxi_proposal (1).docx').ToString())
+$entry = $zip.Entries | Where-Object { $_.FullPath -eq 'word/document.xml' }
+$stream = $entry.Open()
+$reader = New-Object System.IO.StreamReader($stream)
+$content = $reader.ReadToEnd()
+$reader.Close()
+$stream.Close()
+$zip.Dispose()
+$content -replace '<[^>]+>', ''
